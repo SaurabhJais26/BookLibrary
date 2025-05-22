2ec204a6de83d5705f24a967d29aea9ba1d86f80
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-05-22 21:15:58 +0530

    Initial Commit

---
+++ BookLibrary/BookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLibraryApp.swift
+//  BookLibrary
+//
+//  Created by Saurabh Jaiswal on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookLibrary
+//
+//  Created by Saurabh Jaiswal on 22/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
